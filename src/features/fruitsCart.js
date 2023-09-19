ce4ad0fa25d2085efa7702ae42ad0014cf02f9d4
Tresor-ilunga <19im065@esisalama.org>
2023-09-19 21:53:35 +0200

    fix(app) Product deletions and additions

--- src/features/fruitsCart.js
+++ src/features/fruitsCart.js
@@ -11,10 +11,10 @@ export const fruitsCart = createSlice({
     initialState,
     reducers: {
         addOne: (state, action) => {
-            const fruitIndex = state.cart.findIndex(obj => obj.id === action.payload.id)
+            const fruityIndex = state.cart.findIndex(obj => obj.id === action.payload.id)
 
-            if(fruitIndex !== -1){
-                state.cart[fruitIndex].quantity++
+            if(fruityIndex !== -1){
+                state.cart[fruityIndex].quantity++
             }
             else {
                 state.cart.push({...action.payload, quantity: 1})
@@ -23,11 +23,11 @@ export const fruitsCart = createSlice({
         removeOne: (state, action) => {
             const fruit = state.cart.find(obj => obj.id === action.payload)
 
-            if(fruit) {
+            if(fruit){
                 if(fruit.quantity === 1){
                     state.cart = state.cart.filter(fruit => fruit.id !== action.payload)
                 }
-                else {
+                else{
                     fruit.quantity--
                 }
             }
--- src/pages/Fruity/FruityCart.jsx
+++ src/pages/Fruity/FruityCart.jsx
@@ -8,18 +8,7 @@ export default function FruityCart() {
         <div className="bg-slate-100 rounded">
             <p className="text-xl p-5 border-b border-slate-400">Your fruityCart</p>
             <ul>
-                {fruityCart.cart.length > 0 && fruityCart.cart.map(fruit => (
-                    <li
-                    className="px-5 py-2 text-xl"
-                    key={fruit.id}
-                    >
-                        <span className="font-semibold mr-2">{fruit.quatity}</span>
-                        <span>{fruit.name}</span>
-                    </li>
-                ))}
-                {fruityCart.cart.length === 0 && (
-                    <li className="px-5 py-2 text-xl font-semibold">... Grab one fruit !</li>
-                )}
+                
             </ul>
             <p className="text-xl p-5 border-t border-slate-400">
                 Total price : 
