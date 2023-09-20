54365fe71af7797708ca4e3ac81a30fb15ee9b71
Tresor-ilunga <19im065@esisalama.org>
2023-09-20 09:34:55 +0200

    fix(app): Display selected fruits

--- src/pages/Fruity/FruityCart.jsx
+++ src/pages/Fruity/FruityCart.jsx
@@ -8,10 +8,23 @@ export default function FruityCart() {
         <div className="bg-slate-100 rounded">
             <p className="text-xl p-5 border-b border-slate-400">Your fruityCart</p>
             <ul>
-                
+                {fruityCart.cart.length > 0 && fruityCart.cart.map(fruit => (
+                    <li
+                    className="px-5 py-2 text-xl"
+                    key={fruit.id}
+                    >
+                        <span className="font-semibold mr-2">{fruit.quantity}</span>
+                        <span>{fruit.name}</span>
+                    </li>
+                ))}
+                {fruityCart.cart.length === 0 && (
+                    <li className="px-5 py-2 text-xl font-semibold">... Grab one fruit !</li>
+                )}
             </ul>
             <p className="text-xl p-5 border-t border-slate-400">
-                Total price : 
+                Total price :{fruityCart.cart.length > 0 && (
+                    <span className="font-bold ml-2">{fruityCart.cart.reduce((acc, item) => item.price * item.quantity + acc, 0)}$</span>
+                )}
             </p>
         </div>
     )
